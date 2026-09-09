faa600da4409b79cc16da6c6092ff8f27494380d
edward <edwardlfish@gmail.com>
2026-09-09 21:01:17 +0300

    First commit

---
+++ src/service/studentService.js
@@ -0,0 +1,31 @@
+export const addStudent = async student => {
+    // TODO: Implement addStudent, return true if student was added successfully and false otherwise
+}
+
+export const findStudent = async id => {
+    // TODO: Implement findStudent
+}
+
+export const deleteStudent = async id => {
+    // TODO: Implement deleteStudent
+}
+
+export const updateStudent = async (id, data) => {
+    // TODO: Implement updateStudent
+}
+
+export const addScore = async (id, exam, score) => {
+    // TODO: Implement addScore
+}
+
+export const findStudentsByName = async name => {
+    // TODO: Implement findStudentsByName
+}
+
+export const countStudentsByNames = async names => {
+    // TODO: Implement countStudentsByNames
+}
+
+export const findStudentsByMinScore = async (exam, minScore) => {
+    // TODO: Implement findStudentsByMinScore
+}
